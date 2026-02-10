ee25cdd2432395af52bd21ef831889412bd75fdf
periabdraimova03 <peri.abdraimova@gmail.com>
2026-02-09 20:04:26 -0600

    aded

--- README.md
+++ README.md
@@ -3,4 +3,5 @@
 ```hcl
 region = "us-east-1"
 vpc_cidr = "10.0.0.0/16"
+subnet1_cidr = "10.0.1.0/24"
 ```
--- main.tf
+++ main.tf
@@ -12,3 +12,14 @@ resource "aws_vpc" "main" {
 
 variable region {}
 variable vpc_cidr {}
+
+resource "aws_subnet" "main" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = var.subnet1_cidr
+  availability_zone = "us-east-1a"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "subnet1"
+  }
+}
